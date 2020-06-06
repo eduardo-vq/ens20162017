1dc95e2a297368ad5fefc2f6628ddf57ae1af572
Eduardo Peña <eduardorpo@ug.uhile.cl>
2020-06-05 23:59:45 -0400

    ENS

--- Scripts/ENS processing + analysis.R
+++ Scripts/ENS processing + analysis.R
@@ -14,20 +14,21 @@ library(haven)
 # Cargar Bases de datos
 
 #Base de datos Formulario 1 y 2
-ensf1_2 <- read_dta("Data/InputData/ENS_F1_2_.dta")
+ensf1_2dta <- read_dta("Data/InputData/ENS_F1_2_.dta")
 ensf1_2sav <- read_spss("Data/InputData/ENS_F1_2_.sav")
 
 #Base de datos con variables complejas de Formulario 1 y 2
 ens_complejo <- read_spss("Data/InputData/ENS_F1_2_Comuna.sav")
 
 #Explorar bases de datos
-names(ensf1_2)
+names(ensf1_2dta)
+names(ensf1_2sav)
 names(ens_complejo)
 
-#Comparar variables 
-library(arsenal)
+#Comparar variables y Unir DataFrames 
+library(arsenal)#Cargar Paquete
 comparedf(ensf1_2sav,ens_complejo)
-ens <- merge(ensf1_2sav,ens_complejo, all=TRUE) #Merge 
+ens <- merge(ensf1_2sav,ens_complejo, all=TRUE) 
 
 #Seleccionar variables de interés
 library(dplyr)
@@ -43,7 +44,7 @@ class(ENS_analisis$sexo)
 ENS_analisis$sexo <- as.numeric(ENS_analisis$sexo) 
 
 ENS_analisis <- mutate(ENS_analisis, sexo_factor = factor(ENS_analisis$sexo,labels = c("Hombre", "Mujer"))) #queda como objeto factor
-ENS_analisis$sexo_factor #visualizar datos concretos guardados
+ENS_analisis$sexo_factor #visualizar datos 
 table(ENS_analisis$sexo_factor)
 
 # Comuna ()
@@ -74,7 +75,7 @@ table(ENS_analisis$edad_rec65factor)
 
 #Enfermedad pulmonar crónica
 
-#escribir 1.si---y a qué lo quiero cambiar
+#¿Alguna vez un doctor o médico le ha dicho que tiene o que padece de Bronquitis crónica, enfisema pulmonar, enfermedad pulmonar obstructiva crónica o EPOC? -8888 NO SABE /  1 SÍ  / 2 NO
 summary(ENS_analisis$plmnr_cronc)
 table(ENS_analisis$plmnr_cronc)
 class(ENS_analisis$plmnr_cronc)
@@ -82,15 +83,18 @@ ENS_analisis$plmnr_cronc <- as.numeric(ENS_analisis$plmnr_cronc)
 
 ENS_analisis$plmnr_cronc <- car::recode(ENS_analisis$plmnr_cronc, "1=1;2=0; -8888=NA")
 
-
 #Convertir a factor para poner etiquetas
-ENS_analisis$plmnr_cronc_factor<- factor(ENS_analisis$plmnr_cronc, labels= c("Sí", "No"))
+ENS_analisis$plmnr_cronc_factor<- factor(ENS_analisis$plmnr_cronc, labels= c("No", "Sí"))
 table(ENS_analisis$plmnr_cronc_factor)
 
+#Filtrar casos con Enfermdad pulmonar crónica 
+EPC <- ENS_analisis %>%
+  filter(plmnr_cronc == 1)
+  dim(EPC)
 
 #Asma
 
-#escribir 1.si---y a qué lo quiero cambiar
+#¿Alguna vez un doctor o médico le ha dicho que tiene o que padece de Asma? -8888 NO SABE /  1 SÍ  / 2 NO
 summary(ENS_analisis$asma)
 table(ENS_analisis$asma)
 class(ENS_analisis$asma)
@@ -98,30 +102,39 @@ ENS_analisis$asma <- as.numeric(ENS_analisis$asma)
 
 ENS_analisis$asma <- car::recode(ENS_analisis$asma, "1=1;2=0; -8888=NA")
 
-
 #Convertir a factor para poner etiquetas
-ENS_analisis$asma_factor<- factor(ENS_analisis$asma, labels= c("Sí", "No"))
+ENS_analisis$asma_factor<- factor(ENS_analisis$asma, labels= c("No", "Sí"))
 table(ENS_analisis$asma_factor)
 
-#Hipertensión arterial
-#1 SÍ, UNA SOLA VEZ 2 SÍ, MÁS DE UNA VEZ  3 NO, NUNCA ME LO HAN DICHO 4 NO RECUERDO, NO ESTOY SEGURO(A)
-#escribir 1.si---y a qué lo quiero cambiar
+#Filtrar casos con Asma 
+ASMA <- ENS_analisis %>%
+  filter(asma == 1)
+dim(ASMA)
+
+#Hipertensión arterial 
+#ENS: "¿Alguna vez un profesional de la salud le ha diagnosticado presión alta?": 1 SÍ, UNA SOLA VEZ 2 SÍ, MÁS DE UNA VEZ  3 NO, NUNCA ME LO HAN DICHO 4 NO RECUERDO, NO ESTOY SEGURO(A)
+#Recordificar 1 y 2 a Sí, 3 y 4 a No.
 summary(ENS_analisis$hta)
 table(ENS_analisis$hta)
 class(ENS_analisis$hta)
 ENS_analisis$hta <- as.numeric(ENS_analisis$hta)
 
-ENS_analisis <- mutate(ENS_analisis, hta_rec = car::recode(ENS_analisis$hta, "1:2=1;2:4=0; -8888=NA"))
-
+ENS_analisis <- mutate(ENS_analisis, hta_rec = car::recode(ENS_analisis$hta, "1:2=1;3:4=0; -8888=NA"))
+table(ENS_analisis$hta_rec)
 
 #Convertir a factor para poner etiquetas
-ENS_analisis$hta_factor<- factor(ENS_analisis$hta_rec, labels= c("Sí", "No"))
+ENS_analisis$hta_factor<- factor(ENS_analisis$hta_rec, labels= c("No", "Sí"))
 table(ENS_analisis$hta_factor)
+
+#Filtrar casos con Asma 
+HTA <- ENS_analisis %>%
+  filter(hta_rec == 1)
+dim(HTA)
 
 
 saveRDS(ENS_analisis, file = "Data/AnalysisData/ENS20162017_seleccion.RDS")
-
-# ---- 2. CONSTRUIR RESULTADOS DE NIVEL MUESTRAL ----
+D
+# ---- 2. RESULTADOS  ----
 
 library(summarytools)
 
